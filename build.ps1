f96f67f59cd7d49b35fc46ba517edd46b011fca1
ricksterhd123 <ricksterhd123@gmail.com>
2022-02-28 19:51:25 +0000

    Add comments to build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+# Zip folder containing lambda function
 Compress-Archive -Path blog-api/* -DestinationPath terraform/blog-api.zip
+# Init terraform
 terraform -chdir=terraform/ init
+# Deploy
 terraform -chdir=terraform/ apply -auto-approve
