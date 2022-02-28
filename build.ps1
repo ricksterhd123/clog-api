7e2c18e8a342a134ebe14f49b7ae5b142cda0d5a
ricksterhd123 <ricksterhd123@gmail.com>
2022-02-28 19:53:38 +0000

    Add note

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 # Zip folder containing lambda function
+# Output must be named 'blog-api.zip' which is declared in main.tf  
 Compress-Archive -Path blog-api/* -DestinationPath terraform/blog-api.zip
 # Init terraform
 terraform -chdir=terraform/ init
