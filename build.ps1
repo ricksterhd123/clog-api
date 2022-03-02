32bfc194cdc9f506aefeca731b5e5eb762eda894
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-02 04:13:08 +0000

    Always zip the folder on build

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+if (Test-Path -Path terraform/blog-api.zip)
+{
+    Remove-Item terraform/blog-api.zip
+}
 # Zip folder containing lambda function
 # Output must be named 'blog-api.zip' which is declared in main.tf  
 Compress-Archive -Path blog-api/* -DestinationPath terraform/blog-api.zip
