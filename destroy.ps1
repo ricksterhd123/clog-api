77f6c6193666c63184fd296a3cb1e95b6974f293
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-02 04:13:24 +0000

    Remove zip file after destroyed

--- destroy.ps1
+++ destroy.ps1
@@ -1,1 +1,5 @@
-terraform -chdir=terraform/ destroy -auto-approve
+if (Test-Path -Path terraform/blog-api.zip)
+{
+    terraform -chdir=terraform/ destroy -auto-approve
+    Remove-Item terraform/blog-api.zip
+}
