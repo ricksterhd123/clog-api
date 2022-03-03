d5c3148df8e949d36274c0b98d1c1138faeefea1
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-03 23:33:50 +0000

    Create dynamoDB and add policies to lambda role to access the database, add some variables and run terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>3.27"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.27"
     }
+  }
 
-    backend "s3" {
-        bucket = "dzwcq2zr7yfy4mhz"
-        key = "terraform-state/blog-api"
-        region = "eu-west-2"
-    }
-    required_version = ">= 1.1.0"
+  backend "s3" {
+    bucket = "dzwcq2zr7yfy4mhz"
+    key    = "terraform-state/blog-api"
+    region = "eu-west-2"
+  }
+  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
-    profile = "default"
-    region = "eu-west-2"
+  profile = "default"
+  region  = var.region
 }
 
 #
@@ -24,18 +24,18 @@ provider "aws" {
 #
 
 resource "aws_cognito_user_pool" "pool" {
-  name = "blog-api"
+  name = var.project_name
 }
 
 # Creates a URL that users can authorize and get tokens
 resource "aws_cognito_user_pool_domain" "main" {
-  domain       = "blog-api"
+  domain       = var.project_name
   user_pool_id = aws_cognito_user_pool.pool.id
 }
 
 resource "aws_cognito_user_pool_client" "client" {
-  name = "blog-api-client"
-  user_pool_id = aws_cognito_user_pool.pool.id
+  name                                 = "${var.project_name}-client"
+  user_pool_id                         = aws_cognito_user_pool.pool.id
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows = [
     "code"
@@ -59,53 +59,121 @@ resource "aws_cognito_user_pool_client" "client" {
 }
 
 #
+# DynamoDB
+#
+
+resource "aws_dynamodb_table" "blog_api_db" {
+  name           = var.dynamodb_table_name
+  hash_key       = var.dynamodb_hash_key_name
+  range_key      = var.dynamodb_sort_key_name
+  write_capacity = 1
+  read_capacity  = 1
+
+  attribute {
+    name = var.dynamodb_hash_key_name
+    type = "S"
+  }
+
+  attribute {
+    name = var.dynamodb_sort_key_name
+    type = "S"
+  }
+
+  tags = {
+    Name = "${var.project_name}-db"
+  }
+}
+
+#
 # Lambda functions
 #
 
 # Lambda function role
 resource "aws_iam_role" "blog_api_role" {
-    name = "blog-api-role"
+  name = "${var.project_name}-role"
 
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Action = "sts:AssumeRole"
-                Effect = "Allow"
-                Principal = {
-                    Service = "lambda.amazonaws.com"
-                }
-            }
-        ]
-    })
-
-    managed_policy_arns = [
-      "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+      {
+        Action = "dynamodb:PutItem"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Resource = aws_dynamodb_table.blog_api_db.arn
+      },
+      {
+        Action = "dynamodb:Query"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Resource = aws_dynamodb_table.blog_api_db.arn
+      },
+      {
+        Action = "dynamodb:UpdateItem"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Resource = aws_dynamodb_table.blog_api_db.arn
+      },
+      {
+        Action = "dynamodb:GetItem"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Resource = aws_dynamodb_table.blog_api_db.arn
+      },
+      {
+        Action = "dynamodb:DeleteItem"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Resource = aws_dynamodb_table.blog_api_db.arn
+      }
     ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
 }
 
 # API lambda function
 resource "aws_lambda_function" "blog_api" {
-    filename = "blog-api.zip"
-    function_name = "blog-api"
-    role = aws_iam_role.blog_api_role.arn
-    handler = "index.handler"
+  filename      = "${var.project_name}.zip"
+  function_name = "${var.project_name}-lambda"
+  role          = aws_iam_role.blog_api_role.arn
+  handler       = "index.handler"
 
-    source_code_hash = filebase64sha256("blog-api.zip")
-    runtime = "nodejs14.x"
+  source_code_hash = filebase64sha256("${var.project_name}.zip")
+  runtime          = "nodejs14.x"
 
-    environment {
-        variables = {
-            foo = "bar"
-        }
+  environment {
+    variables = {
+      BLOG_API_DYNAMODB_TABLE_NAME = var.dynamodb_table_name
+      BLOG_API_dynamodb_hash_key_name = var.dynamodb_hash_key_name
+      REGION                       = var.region
     }
+  }
 }
 
 resource "aws_lambda_permission" "api-gateway" {
-  action = "lambda:InvokeFunction"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.blog_api.function_name
-  principal = "apigateway.amazonaws.com"
-  source_arn = "${aws_apigatewayv2_api.gateway.execution_arn}/*/$default"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.gateway.execution_arn}/*/$default"
 }
 
 #
@@ -113,7 +181,7 @@ resource "aws_lambda_permission" "api-gateway" {
 #
 
 resource "aws_apigatewayv2_api" "gateway" {
-  name = "blog-api-gateway"
+  name          = "${var.project_name}-gateway"
   protocol_type = "HTTP"
 }
 
@@ -131,25 +199,25 @@ resource "aws_apigatewayv2_authorizer" "auth" {
 
 # Link the API with a lambda function
 resource "aws_apigatewayv2_integration" "int" {
-  api_id           = aws_apigatewayv2_api.gateway.id
-  integration_type = "AWS_PROXY"
-  connection_type = "INTERNET"
-  integration_method = "POST"
-  integration_uri = aws_lambda_function.blog_api.invoke_arn
+  api_id                 = aws_apigatewayv2_api.gateway.id
+  integration_type       = "AWS_PROXY"
+  connection_type        = "INTERNET"
+  integration_method     = "POST"
+  integration_uri        = aws_lambda_function.blog_api.invoke_arn
   payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "route" {
-  api_id    = aws_apigatewayv2_api.gateway.id
-  route_key = "$default"
-  target = "integrations/${aws_apigatewayv2_integration.int.id}"
+  api_id             = aws_apigatewayv2_api.gateway.id
+  route_key          = "$default"
+  target             = "integrations/${aws_apigatewayv2_integration.int.id}"
   authorization_type = "JWT"
-  authorizer_id = aws_apigatewayv2_authorizer.auth.id
+  authorizer_id      = aws_apigatewayv2_authorizer.auth.id
 }
 
 resource "aws_apigatewayv2_stage" "stage" {
-  api_id = aws_apigatewayv2_api.gateway.id
-  name = "dev"
+  api_id      = aws_apigatewayv2_api.gateway.id
+  name        = "dev"
   auto_deploy = true
 }
 
