f2f203866e48b8ad9a09768eccd4660516c9f6f5
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-04 20:57:14 +0000

    Generate UUID for namespace

--- blog-api/models/article.js
+++ blog-api/models/article.js
@@ -2,6 +2,7 @@ const { v5 } = require('uuid');
 const dynamoDb = require('./dynamoDb');
 
 const typeName = 'ARTICLE';
+const NAMESPACE = 'f32ddcff-9177-4d3e-8cb5-efd0fad3b401';
 
 async function create(author, title, body, tags) {
     if (!(author && title && body)) {
@@ -17,7 +18,7 @@ async function create(author, title, body, tags) {
         tags,
     };
 
-    Item[id] = v5(title, author);
+    Item[id] = v5(title, NAMESPACE);
     Item[type] = typeName;
 
     return dynamoDb.put(Item);
