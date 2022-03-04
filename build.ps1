dedbea2eaad5456b65aea3782830a07669c6d88a
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-04 00:31:44 +0000

    Use 7zip instead and do npm install beforehand

--- build.ps1
+++ build.ps1
@@ -2,9 +2,14 @@ if (Test-Path -Path terraform/blog-api.zip)
 {
     Remove-Item terraform/blog-api.zip
 }
+Set-Location -Path blog-api/
+# install modules
+npm install
+# Go back
+Set-Location -Path ../
 # Zip folder containing lambda function
 # Output must be named 'blog-api.zip' which is declared in main.tf  
-Compress-Archive -Path blog-api/* -DestinationPath terraform/blog-api.zip
+7z a -tzip ./terraform/blog-api.zip ./blog-api/*
 # Init terraform
 terraform -chdir=terraform/ init
 # Deploy
