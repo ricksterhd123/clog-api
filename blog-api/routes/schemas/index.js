b2cb384f12959a605d3041ab4a3274f98bc3a18f
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-04 20:21:05 +0000

    provide validateArticle function

--- blog-api/routes/schemas/index.js
+++ blog-api/routes/schemas/index.js
@@ -1,1 +1,9 @@
-// TODO: Provide functions that validate against json schemas
+const Ajv = require('ajv');
+const articleSchema = require('./article.json');
+
+const ajv = new Ajv();
+const validateArticle = ajv.compile(articleSchema);
+
+module.exports = {
+    validateArticle,
+};
