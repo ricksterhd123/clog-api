eb9e2d0d19cf2e460499212d0b2289a1617bf5c7
ricksterhd123 <ricksterhd123@gmail.com>
2022-03-06 10:34:46 +0000

    Put verifier inside isJwtValid function

--- blog-api/utils/jwt.js
+++ blog-api/utils/jwt.js
@@ -1,12 +1,12 @@
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
 
-const verifier = CognitoJwtVerifier.create({
-    userPoolId: process.env.USER_POOL_ID,
-    tokenUse: 'access',
-    clientId: process.env.USER_POOL_APP_CLIENT_ID,
-});
-
 async function isJwtValid(jwt) {
+    const verifier = CognitoJwtVerifier.create({
+        userPoolId: process.env.USER_POOL_ID,
+        tokenUse: 'access',
+        clientId: process.env.USER_POOL_APP_CLIENT_ID,
+    });
+
     try {
         return verifier.verify(jwt);
     } catch {
